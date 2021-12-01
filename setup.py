c5ceef6a397e154389f715c723dc761c6f2dcebc
ti-vo <teresa.vogl@tropos.de>
2021-12-01 19:22:54 +0100

    changed folder structure

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-
-
 from setuptools import setup, find_packages
 
 with open("README.md") as readme:
     long_description = readme.read()
 
+version = {}
 setup(
     name='pyPEAKO',  # pip install pypeako
     version='0.0.3.post2',
@@ -15,8 +14,10 @@ setup(
     author_email='teresa.vogl@uni-leipzig.de',
     url='https://github.com/ti-vo/pyPEAKO',
     license='MIT',
-    package_dir={'': 'src'},
-    py_modules=['pypeako'], #find_packages(exclude=['docs', 'tests', 'playground']),
+    include_package_data=True,
+    packages=find_packages(),
+#    package_dir={'': 'pypeako'},
+#    py_modules=['pypeako'], #find_packages(exclude=['docs', 'tests', 'playground']),
 #    package_data={'pyPEAKO': ['../examples/sample_spectra.nc', '../examples/marked_peaks_sample_spectra.nc']},
 #    include_package_data=True,
     python_requires='>=3.6',
@@ -24,6 +25,7 @@ setup(
                       'matplotlib>=3.0.2', 'xarray', 'requests', 'dask[complete]'],
     extras_require={'dev': ['pytest>=3.7', 'check-manifest', 'twine'], },
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
